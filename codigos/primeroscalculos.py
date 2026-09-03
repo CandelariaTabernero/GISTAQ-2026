b13965afca7ffc3e4b523ef4051c8b334a818c89
CandelariaTabernero <candeee120505@gmail.com>
2026-09-03 17:40:11 -0300

    bandas recortadas

--- codigos/primeroscalculos.py
+++ codigos/primeroscalculos.py
@@ -41,3 +41,59 @@ with rio.open(ruta_banda3) as src:
     
     print("¡Recorte realizado con éxito!")
     print("Nueva forma (shape) de la matriz recortada:", imagen_recortada.shape)
+
+    
+# LECTURA Y RECORTE DE LA BANDA 6 (Infrarrojo de onda corta - SWIR 1)
+
+# Apuntamos a la ruta exacta de la Banda 6
+ruta_banda6 = "raster/LC08_L2SP_226079_20260125_20260130_02_T1/LC08_L2SP_226079_20260125_20260130_02_T1_SR_B6.TIF"
+
+with rio.open(ruta_banda6) as src6:
+    # Recortamos usando la misma geometría del vector que ya transformamos antes
+    imagen_recortada_b6, transform_b6 = mask(src6, geometria_vector, crop=True)
+    
+    print("¡Banda 6 recortada con éxito!")
+    print("Nueva forma (shape) de la Banda 6:", imagen_recortada_b6.shape)
+
+
+# LECTURA Y RECORTE DE LA BANDA 4 (Rojo)
+
+ruta_banda4 = "raster/LC08_L2SP_226079_20260125_20260130_02_T1/LC08_L2SP_226079_20260125_20260130_02_T1_SR_B4.TIF"
+
+with rio.open(ruta_banda4) as src4:
+    # 1. Reproyectamos el vector al sistema de coordenadas de la banda 4
+    vector_reproyectado_4 = vector.to_crs(src4.crs)
+    
+    # 2. Recortamos usando la máscara y el crop
+    imagen_recortada_b4, _ = mask(src4, vector_reproyectado_4.geometry, crop=True)
+    
+    print("¡Banda 4 recortada con éxito!")
+    print("Nueva forma (shape) de la Banda 4:", imagen_recortada_b4.shape)
+
+# LECTURA Y RECORTE DE LA BANDA 5 (Infrarrojo cercano - NIR)
+
+ruta_banda5 = "raster/LC08_L2SP_226079_20260125_20260130_02_T1/LC08_L2SP_226079_20260125_20260130_02_T1_SR_B5.TIF"
+
+with rio.open(ruta_banda5) as src5:
+    # 1. Reproyectamos el vector al sistema de coordenadas de la banda 5
+    vector_reproyectado_5 = vector.to_crs(src5.crs)
+    
+    # 2. Recortamos usando la máscara
+    imagen_recortada_b5, _ = mask(src5, vector_reproyectado_5.geometry, crop=True)
+    
+    print("¡Banda 5 recortada con éxito!")
+    print("Nueva forma (shape) de la Banda 5:", imagen_recortada_b5.shape)
+
+# LECTURA Y RECORTE DE LA BANDA 7 (Infrarrojo de onda corta 2 - SWIR 2)
+
+ruta_banda7 = "raster/LC08_L2SP_226079_20260125_20260130_02_T1/LC08_L2SP_226079_20260125_20260130_02_T1_SR_B7.TIF"
+
+with rio.open(ruta_banda7) as src7:
+    # 1. Reproyectamos el vector al sistema de coordenadas de la banda 7
+    vector_reproyectado_7 = vector.to_crs(src7.crs)
+    
+    # 2. Recortamos usando la máscara
+    imagen_recortada_b7, _ = mask(src7, vector_reproyectado_7.geometry, crop=True)
+    
+    print("¡Banda 7 recortada con éxito!")
+    print("Nueva forma (shape) de la Banda 7:", imagen_recortada_b7.shape)
